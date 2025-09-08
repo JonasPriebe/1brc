dbac27eedc46edac13f93a11de0a50b8bc9b1f2f
JonasPriebe <priebe.j@yahoo.com>
2025-09-08 10:31:12 +0200

    multithreading

--- 1brc/IO/IO.cs
+++ 1brc/IO/IO.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics.CodeAnalysis;
 using System.IO.MemoryMappedFiles;
 using System.Text;
 
@@ -6,7 +5,6 @@ namespace _1brc.IO;
 
 public class IO
 {
-    [SuppressMessage("ReSharper.DPA", "DPA0000: DPA issues")]
     public static void ReadContents(FileInfo file)
     {
         if (file == null)
@@ -15,53 +13,86 @@ public class IO
         if (file.Exists == false)
             throw new FileNotFoundException("Couldn't find input file at: " + file.FullName);
 
-        var fileLength = file.Length;
-
         using var mmf = MemoryMappedFile.CreateFromFile(file.FullName, FileMode.Open, null, 0, MemoryMappedFileAccess.Read);
-        using var accessor = mmf.CreateViewAccessor(0, fileLength, MemoryMappedFileAccess.Read);
-        var chunksize = 64000;
-        var byteBuffer = new byte[chunksize+4];
-        var charBuffer = new char[chunksize];
-        var leftoverLength = 0;
-        long lineCounter = 0;
-        for (long offset = 0; offset < fileLength; offset += chunksize )
+        
+        var maxThreads = Environment.ProcessorCount;
+        Console.WriteLine("System has '" + maxThreads + " usable Processors.");
+        
+        var fileLength = file.Length;
+        var parts = fileLength / maxThreads;
+        var threads = new Thread[maxThreads];
+        var threadCounts = new int[maxThreads];
+        
+        for (var t = 0; t < maxThreads; t++)
         {
-            var bytesToRead = (int)Math.Min(chunksize, fileLength - offset);
-            accessor.ReadArray(offset, byteBuffer, leftoverLength, bytesToRead);
+            var idx = t;
+            var start = parts * idx;
+            var length = idx == maxThreads - 1 ? fileLength - start : parts;
             
+            var accessor = mmf.CreateViewAccessor(start, length, MemoryMappedFileAccess.Read);
+            
+            threads[t] = new Thread(() =>
+            {
+                var threadRes = EntryParse(length, accessor);
+                threadCounts[idx] = threadRes;
+            });
+            threads[t].Start();
+            Console.WriteLine("Thread started with idx: " + idx);
+        }
+
+        for (var index = 0; index < threads.Length; index++)
+        {
+            threads[index].Join();
+            Console.WriteLine("Thread " + index + " has read '" + threadCounts[index] + "' lines.");
+        }
+
+        var total = threadCounts.Sum();
+        Console.WriteLine("total lines read after Join: " + total);
+    }
+
+    private static int EntryParse(long accessorLength, MemoryMappedViewAccessor accessor)
+    {
+        var chunkSize = 64000;
+        var byteBuffer = new byte[chunkSize + 4];
+        var charBuffer = new char[chunkSize];
+        var leftoverLength = 0;
+        var lineCounter = 0;
+        for (long offset = 0; offset < accessorLength; offset += chunkSize)
+        {
+            var bytesToRead = (int)Math.Min(chunkSize, accessorLength - offset);
+            accessor.ReadArray(offset, byteBuffer, leftoverLength, bytesToRead);
+
             var totalBytes = leftoverLength + bytesToRead;
             ReadOnlySpan<byte> spanToDecode = byteBuffer.AsSpan(0, totalBytes);
-            
-            Decoder decoder = Encoding.UTF8.GetDecoder();
+
+            var decoder = Encoding.UTF8.GetDecoder();
             decoder.Convert(spanToDecode.ToArray(), 0, spanToDecode.Length, charBuffer, 0, charBuffer.Length, false,
-                out int bytesUsed, out int charsUsed, out bool completed);
-            
+                out var bytesUsed, out var charsUsed, out var completed);
+
             leftoverLength = totalBytes - bytesUsed;
             if (leftoverLength > 0)
                 Buffer.BlockCopy(byteBuffer, bytesUsed, byteBuffer, 0, leftoverLength);
-            
-            int lineStart = 0;
+
+            var lineStart = 0;
             for (var i = 0; i < charsUsed; i++)
             {
-                if (charBuffer[i] == '\n')
-                {
-                    ReadOnlySpan<char> lineSpan = charBuffer.AsSpan(lineStart, i - lineStart);
-                    Console.WriteLine(lineSpan.ToString());
-                    lineCounter++;
-                    lineStart = i + 1;
-                }
+                if (charBuffer[i] != '\n') continue;
+                // TODO: Add Processing, min, max, mean
+                ReadOnlySpan<char> lineSpan = charBuffer.AsSpan(lineStart, i - lineStart);
+                lineCounter++;
+                lineStart = i + 1;
             }
+
             var partialLength = charsUsed - lineStart;
             if (partialLength > 0)
                 Buffer.BlockCopy(charBuffer, lineStart * sizeof(char), charBuffer, 0, partialLength * sizeof(char));
         }
-        if (leftoverLength > 0)
-        {
-            ReadOnlySpan<char> finalLine = charBuffer.AsSpan(0, leftoverLength);
-            lineCounter++;
-        }
 
-        Console.WriteLine("Total lines: " + lineCounter);
+        if (leftoverLength <= 0) return lineCounter;
+        // TODO: Add Processing, min, max, mean
+        ReadOnlySpan<char> finalLine = charBuffer.AsSpan(0, leftoverLength);
+        lineCounter++;
+
+        return lineCounter;
     }
-    
 }
