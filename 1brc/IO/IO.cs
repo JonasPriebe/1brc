626475aa1571fad1c8e0a79b7f8821b54452e501
JonasPriebe <priebe.j@yahoo.com>
2025-09-14 14:23:03 +0200

    rewrite entryparse logic, to avoid bad allcoation and accesses

--- 1brc/IO/IO.cs
+++ 1brc/IO/IO.cs
@@ -1,4 +1,5 @@
 using System.Collections.Concurrent;
+using System.Formats.Tar;
 using System.Globalization;
 using System.IO.MemoryMappedFiles;
 using System.Text;
@@ -22,9 +23,7 @@ public class IO
         file.Refresh();
         if (file.Exists == false)
             throw new FileNotFoundException("Couldn't find input file at: " + file.FullName);
-        byte[] data = File.ReadAllBytes(file.FullName);
-        // TODO: find solution, Memory<byte> only supports files up to 2gb 
-        Memory<byte> memory = data;
+        using var mmf = MemoryMappedFile.CreateFromFile(file.FullName, FileMode.Open, null, file.Length, MemoryMappedFileAccess.Read);
 
         var maxThreads = Environment.ProcessorCount;
         Console.WriteLine("System has '" + maxThreads + " usable Processors.");
@@ -35,12 +34,16 @@ public class IO
         
         var threadCounts = new int[maxThreads];
         var threadDicts = new Dictionary<string, Accumulator>[maxThreads];
-        var stack = new ConcurrentQueue<Memory<byte>>();
+        for (int i = 0; i < threadDicts.Length; i++)
+        {
+            threadDicts[i] = new Dictionary<string, Accumulator>();
+        }
+        var stack = new ConcurrentQueue<MemoryMappedViewAccessor>();
         
-        foreach (var slice in SplitByLines(memory, chunkSize))
+        foreach (var slice in SplitByLines(mmf, chunkSize, file.Length))
             stack.Enqueue(slice);
         
-        Console.WriteLine(stack.Count);
+        Console.WriteLine("workitems in stack: "+stack.Count);
         
         for (var t = 0; t < maxThreads; t++)
         {
@@ -60,91 +63,55 @@ public class IO
             w.Join();
         
         PrintSummary(threadCounts, threadDicts);
-        for(var i = 0; i < workers.Length; i ++)
+        var sum = 0;
+        for (var i = 0; i < workers.Length; i++)
+        {
             Console.WriteLine("Thread " + i + " has read '" + threadCounts[i] + "' lines.");
+            sum += threadCounts[i];
+        }
+        Console.WriteLine("Total lines read: " + sum);
         
     }
 
-    static int EntryParse(Memory<byte> memory, Dictionary<string, Accumulator> threadDict)
+    static int EntryParse(MemoryMappedViewAccessor memory, Dictionary<string, Accumulator> threadDict)
     {
-        var blockSize = 64 * 1024;
-        var count = 0;
-
-        var bytes = memory.Span;
-        var utf8 = Encoding.UTF8;
-        var decoder = utf8.GetDecoder();
-
-        var byteIndex = 0;
-        var charBuffer = new char[utf8.GetMaxCharCount(blockSize)];
-        var carryLine = new List<char>();
-
-        while (byteIndex < bytes.Length)
-        {
-            var bytesToRead = Math.Min(blockSize, bytes.Length - byteIndex);
-            var decodedChars = decoder.GetChars(bytes.Slice(byteIndex, bytesToRead), charBuffer, false);
-
-            var start = 0;
-            for (var i = 0; i < decodedChars; i++)
-            {
-                if (charBuffer[i] == '\n')
-                {
-                    if (carryLine.Count > 0)
-                    {
-                        carryLine.AddRange(charBuffer.AsSpan(start, i - start));
-                        ProcessLine(carryLine.ToArray(), threadDict);
-                        carryLine.Clear();
-                    }
-                    else
-                    {
-                        ProcessLine(charBuffer.AsSpan(start, i - start), threadDict);
-                    }
-                    count++;
-                    start = i + 1;
-                }
-            }
-
-            if (start < decodedChars)
-                carryLine.AddRange(charBuffer.AsSpan(start, decodedChars - start));
-
-            byteIndex += bytesToRead;
-        }
-
-        if (carryLine.Count <= 0) 
-            return count;
-        
-        ProcessLine(carryLine.ToArray(), threadDict);
-        count++;
-        
-        return count;
+ 
     }
 
     // With naive block partitioning of the original memory <T> i ran into the issue of the blocks not ending on \n
     // this could be managed with overlapping memory<T> splits, but the more elegant approach is to end the blocks 
     // on \n by default, so we don't have to worry about boundaries, which makes the multithreaded scanning easier
-    static Memory<byte>[] SplitByLines(Memory<byte> memory, int chunkSize)
+    static MemoryMappedViewAccessor[] SplitByLines(MemoryMappedFile memory, int chunkSize, long total)
     {
         ArgumentOutOfRangeException.ThrowIfNegativeOrZero(chunkSize);
 
-        var chunks = new List<Memory<byte>>();
-        var total = memory.Length;
-        var start = 0;
+        var chunks = new List<MemoryMappedViewAccessor>();
+        long start = 0;
 
         while (start < total)
         {
-            var end = Math.Min(start + chunkSize, total);
-            var scan = end;
+            long end = Math.Min(start + chunkSize, total);
+            int scanSize = (int)Math.Min(1000, total - end);
+
+            byte[] buffer = new byte[scanSize];
+            if (scanSize > 0)
+            {
+                using var accessor = memory.CreateViewAccessor(end, scanSize, MemoryMappedFileAccess.Read);
+                accessor.ReadArray(0, buffer, 0, scanSize);
+            }
             
-            while (scan < total && memory.Span[scan] != (byte)'\n')
-                scan++;
+            int index = Array.IndexOf(buffer, (byte)'\n');
+            int size = (index != -1) ? (int)(end - start + index + 1) : (int)(end - start + scanSize);
 
-            if (scan < total)
-                scan++;
+            var completeView = memory.CreateViewAccessor(start, size, MemoryMappedFileAccess.Read);
+            chunks.Add(completeView);
 
-            chunks.Add(memory.Slice(start, scan - start));
-            start = scan;
+            start += size;
         }
+
         return chunks.ToArray();
     }
+
 
     static void PrintSummary(int[] threadCounts, Dictionary<string, Accumulator>[] threadDicts)
     {
